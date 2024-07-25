dd116ace89f8f359b24a86f8e69412d917512fef
CodeMaster1129 <codemaster1129@gmail.com>
2024-07-25 15:02:34 -0700

    updated router

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-d
 import { useSelector } from "react-redux";
 import SignUp from '../pages/auth/signup';
 import Signin from '../pages/auth/signin';
+import ForgotPassword from "../pages/auth/forgot-password"
 import Dashboard from '../pages/dashboard';
 import Loader from "../components/common/Loader"
 import { ProtectedRoute, PublicRoute } from "./custom";
@@ -13,8 +14,11 @@ const RootRouter = () => {
      <Router>
       <Routes>
         <Route index element={<Navigate to="auth/signin" />} />
-        <Route path="auth/signin" element={<PublicRoute element={<Signin />} />} />
-        <Route path="auth/signup" element={<PublicRoute element={<SignUp />} />} />
+        <Route path="auth">
+          <Route path="signin" element={<PublicRoute element={<Signin />} />} />
+          <Route path="signup" element={<PublicRoute element={<SignUp />} />} />
+          <Route path="forgot-password" element={<PublicRoute element={<ForgotPassword />} />} />
+        </Route>
         <Route path="dashboard" element={<ProtectedRoute element={<Dashboard />} />} />
         <Route path="*" element={<Navigate to="auth/signin" />} />
       </Routes>
